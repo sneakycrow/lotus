e3029c4eb09eb325834bb3aab5c9f06ad9843be6
zach <zach@isolary.com>
2018-03-24 20:15:09 -0700

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # boiler-plate
 A boiler plate for my React projets
+
+# About
+The app has some pretty nice little things already setup. ESLint, Babel, Webpack 4, and CSS Modules are the core benefits.
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[name]__[local]__[hashLbase64:5]'
+              }
+            },
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'SneakyCrow.io',
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin({ filename: 'app.bundle.css', allChunks: true }),
+  ]
+};
