a15ad3d7cb7b6474bad55189dd30bccff959c874
zach <zach@isolary.com>
2018-04-27 09:59:17 -0700

    total nuke

--- src/containers/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-import style from './App.scss';
-
-// NOTE: You should route all your containers into this final App container
-const App = () => <p className={style.example}>Hello World</p>;
-
-export default hot(module)(App);
