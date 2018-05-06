12346eca1e7f3360b065f8fee080fd1642c31548
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-06 13:37:21 -0700

    Fixed & Added webpack settings
    Added Brotli for minifaction
    Fixed various syntax
    Added new alias for react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BrotliWebpackPlugin = require("brotli-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
   module: {
     rules: [
       {
@@ -10,12 +17,23 @@ module.exports = {
           loader: "babel-loader"
         }
       }
-    ],
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
-      })
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new BrotliWebpackPlugin({
+      asset: "app.bundle.min.js",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "src/components")
+    }
   }
 };
