b8ca1eac8a4afae29caa281944fe6ccf0474b07b
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-06 19:33:55 -0700

    removed default styling

--- src/components/Navigation/NavItem.js
+++ src/components/Navigation/NavItem.js
@@ -6,6 +6,8 @@ import styled from "react-emotion";
 const NavLink = styled(Link)`
   padding: 8px 16px;
   margin: 0 8px;
+  text-decoration: none;
+  color: black;
 `;
 
 const NavItem = ({ name, link }) => <NavLink to={link}>{name}</NavLink>;
