c26376bd07156e80a5963babf01c81777595bcf7
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-06 19:33:29 -0700

    added a pallette

---
+++ src/utils/pallette.js
@@ -0,0 +1,10 @@
+const pallette = {
+  /* RGB */
+  white: "rgba(239, 239, 239, 1)",
+  blue: "rgba(52, 84, 209, 1)",
+  green: "rgba(52, 209, 191, 1)",
+  black: "rgba(7, 7, 7, 1)",
+  red: "rgba(209, 52, 91, 1)"
+};
+
+export default pallette;
