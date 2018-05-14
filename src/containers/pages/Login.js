a6df6d8043f468f6e661ed18e4bd3a93cfd691aa
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-13 17:42:44 -0700

    styled login some more

--- src/containers/pages/Login.js
+++ src/containers/pages/Login.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import styled from "react-emotion";
 import Input from "components/Form/Input";
+import Button from "components/Button";
+import { spacing } from "utils/variables";
 
 const Container = styled("div")`
   display: flex;
   height: 100vh;
   align-items: center;
   justify-content: center;
+`;
+
+const LoginContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  padding: ${spacing.sm};
+  width: 25%;
 `;
 
 class Login extends Component {
@@ -28,18 +37,23 @@ class Login extends Component {
   render() {
     return (
       <Container>
-        <Input
-          name="username"
-          value={this.state.username}
-          placeholder="Username"
-          onChangeHandler={this.onChangeHandler}
-        />
-        <Input
-          name="password"
-          value={this.state.password}
-          placeholder="Password"
-          onChangeHandler={this.onChangeHandler}
-        />
+        <LoginContainer>
+          <h2>Login</h2>
+          <Input
+            name="username"
+            value={this.state.username}
+            placeholder="Username"
+            onChangeHandler={this.onChangeHandler}
+          />
+          <Input
+            name="password"
+            value={this.state.password}
+            placeholder="Password"
+            onChangeHandler={this.onChangeHandler}
+            type="password"
+          />
+          <Button>Submit</Button>
+        </LoginContainer>
       </Container>
     );
   }
