108aa23dbaffda240bd4da034716a9a125f6edb2
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-13 17:41:57 -0700

    added a new color and globalVariables const

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -4,11 +4,17 @@ export const pallette = {
   blue: "rgba(52, 84, 209, 1)",
   green: "rgba(52, 209, 191, 1)",
   black: "rgba(7, 7, 7, 1)",
-  red: "rgba(209, 52, 91, 1)"
+  red: "rgba(209, 52, 91, 1)",
+  gray: "rgba(109, 108, 108, 1)",
+  lightGray: "rgba(140, 140, 140, 1)"
 };
 
 export const spacing = {
   xs: "8px",
   sm: "16px",
   lg: "64px"
+};
+
+export const globalVariables = {
+  fontSize: "16px"
 };
