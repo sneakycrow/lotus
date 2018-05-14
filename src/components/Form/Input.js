3233a0a8c3ae31ad085fe6a9660fa67acc0ddbd2
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-13 17:42:31 -0700

    styled inputs

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "react-emotion";
+import { pallette, spacing, globalVariables } from "utils/variables";
+
+const StyledInput = styled("input")`
+  border-radius: 3px;
+  border: 2px solid ${pallette.gray};
+  padding: ${spacing.xs} ${spacing.xs};
+  font-size: ${globalVariables.fontSize};
+  margin: ${spacing.xs} 0;
+  &::placeholder {
+    font-weight: bold;
+    color: ${pallette.lightGray};
+  }
+`;
 
 const Input = ({ onChangeHandler, value, placeholder, type, name }) => (
-  <input
+  <StyledInput
     name={name}
     type={type}
     onChange={onChangeHandler}
