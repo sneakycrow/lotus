b42f3aa1b8ecaac05240c227807c3bdc3a62a57f
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 11:47:30 -0700

    Removed unneessary styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
 import { hot } from "react-hot-loader";
-import styled from "react-emotion";
 import Header from "containers/Header";
 import { Home, About } from "containers/pages";
-import { spacing } from "utils/variables";
-
-const Main = styled("main")`
-  margin-top: ${spacing.lg};
-`;
 
 const App = () => (
   <Fragment>
     <Header />
-    <Main>
+    <div>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-    </Main>
+    </div>
   </Fragment>
 );
 
