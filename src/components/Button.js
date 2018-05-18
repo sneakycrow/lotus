484bddd4e8e6bbc1cd25915030e0314806f7bb4d
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 10:05:53 -0700

    added dynamic styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,40 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "react-emotion";
-import { spacing } from "utils/variables";
+import styled, { css } from "react-emotion";
+import { darken } from "polished";
+import { spacing, pallette } from "utils/variables";
+
+const types = {
+  primary: css`
+    background-color: ${pallette.primary};
+    border: 1px solid ${darken(0.2, pallette.primary)};
+  `,
+  secondary: css`
+    background-color: transparent;
+    border: 2px solid ${pallette.secondary};
+  `,
+  disabled: css`
+    background-color: ${darken(0.8, pallette.white)};
+    border: 1px solid ${pallette.black};
+  `
+};
 
 const StyledButton = styled("button")`
   width: 128px;
   padding: ${spacing.xs};
   font-weight: 700;
   font-family: "Lato", sans-serif;
-  background-color: ${props => (props.disabled ? `red` : `blue`)};
+  color: ${pallette.white};
+  ${props => types[props.type]};
 `;
 
-const Button = ({ children, onClick, disabled, secondary }) => {
-  // Put some props inside this object, so that we can spread them onto the StyledButton tag below
-  const buttonProps = {
-    disabled,
-    secondary
+const Button = ({ children, onClick, type, isDisabled }) => {
+  const props = {
+    onClick: isDisabled ? () => {} : onClick,
+    type: isDisabled ? "disabled" : type
   };
-  return (
-    <StyledButton onClick={onClick} {...buttonProps}>
-      {children}
-    </StyledButton>
-  );
+  return <StyledButton {...props}>{children}</StyledButton>;
 };
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func,
-  disabled: PropTypes.bool,
-  secondary: PropTypes.bool
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool
 };
 
 Button.defaultProps = {
   onClick: () => {},
-  disabled: false,
-  secondary: false
+  type: "primary",
+  isDisabled: false
 };
 
 export default Button;
