c3e35015d8948f4fa81244ae4fdf2da535299ce6
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 10:07:09 -0700

    adjusted keys and changed the login navitem to a button

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "components/Navigation/Navbar";
 import NavItem from "components/Navigation/NavItem";
+import Button from "components/Button";
 
 const Header = () => {
   const routes = [
-    <NavItem name="Home" link="/" key="1" />,
-    <NavItem name="About" link="/about" key="2" />,
-    <NavItem name="Sign In" link="/login" key="3" type="action" />
+    <NavItem name="Home" link="/" key="home" />,
+    <NavItem name="About" link="/about" key="about" />,
+    <Button key="login">Login</Button>
   ];
   return <Navbar navItems={routes} />;
 };
