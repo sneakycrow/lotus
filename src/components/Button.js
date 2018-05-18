874206fa5295a52953421999103a069f4ddb8bdb
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 11:48:13 -0700

    more small styling updates

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,25 +7,30 @@ import { spacing, pallette } from "utils/variables";
 const types = {
   primary: css`
     background-color: ${pallette.primary};
-    border: 1px solid ${darken(0.2, pallette.primary)};
+    border: 2px solid ${darken(0.2, pallette.primary)};
   `,
   secondary: css`
     background-color: transparent;
     border: 2px solid ${pallette.secondary};
   `,
   disabled: css`
-    background-color: ${darken(0.8, pallette.white)};
-    border: 1px solid ${pallette.black};
+    background-color: ${darken(0.6, pallette.white)};
+    border: 2px solid ${darken(0.8, pallette.white)};
   `
 };
 
 const StyledButton = styled("button")`
   width: 128px;
+  height: 40px;
   padding: ${spacing.xs};
   font-weight: 700;
   font-family: "Lato", sans-serif;
   color: ${pallette.white};
+  text-transform: uppercase;
   ${props => types[props.type]};
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Button = ({ children, onClick, type, isDisabled }) => {
