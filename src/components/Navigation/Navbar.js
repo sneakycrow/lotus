6be9e33e7e6f9faab9cc038df193673881c2f4ab
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 11:34:52 -0700

    refactored navbar for better usage/templating

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,20 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "react-emotion";
+import { darken } from "polished";
+import { spacing, pallette } from "utils/variables";
 
 const Container = styled("div")`
-  width: 100%;
-  position: absolute;
-  top: 0;
   height: 64px;
-  box-shadow: 0 4px 2px #dedede;
+  box-shadow: 0 4px 2px ${darken(0.2, pallette.white)};
+  background-color: ${pallette.white};
+  display: grid;
+  grid-template-columns: 25% auto 25%;
+  padding: ${spacing.xs};
+`;
+
+const Navigation = styled("div")`
+  grid-column-start: 3;
   display: flex;
   align-items: center;
   justify-content: flex-end;
 `;
 
 const Navbar = ({ navItems }) => (
-  <Container>{navItems.map(nav => nav)}</Container>
+  <Container>
+    <Navigation>{navItems.map(nav => nav)}</Navigation>
+  </Container>
 );
 
 Navbar.propTypes = {
