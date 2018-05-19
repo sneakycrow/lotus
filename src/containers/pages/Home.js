e9fbffbfd86b9cf617191042c9466000e34a0be5
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-18 17:50:44 -0700

    added modal control

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "react-emotion";
 import { spacing } from "utils/variables";
+import Modal from "components/Modal";
+import Button from "components/Button";
 
 const Grid = styled("div")`
   display: grid;
@@ -16,11 +18,37 @@ const Sidebar = styled("div")`
   grid-column: 2;
 `;
 
-const Home = () => (
-  <Grid>
-    <Main>Main side rendered</Main>
-    <Sidebar>Sidebar rendered</Sidebar>
-  </Grid>
-);
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isModalShown: false
+    };
+    this.handleToggleModal = this.handleToggleModal.bind(this);
+  }
+
+  handleToggleModal() {
+    this.setState(state => ({ isModalShown: !state.isModalShown }));
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Main>
+          Main side rendered
+          <Modal
+            isModalShown={this.state.isModalShown}
+            handleToggleModal={this.handleToggleModal}
+          >
+            Butts
+          </Modal>
+        </Main>
+        <Sidebar>
+          <Button onClick={this.handleToggleModal}>Toggle Button</Button>
+        </Sidebar>
+      </Grid>
+    );
+  }
+}
 
 export default Home;
