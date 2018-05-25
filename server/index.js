a3d1764b6e1daa36f094f878be3763b2348fe0d7
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-25 10:17:34 -0700

    started setting up backend authentication

--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
 /* eslint-disable no-console */
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const api = require("./routes/api");
 const path = require("path");
 
 // Connect to Mongoose
 const dbURI = process.env.MONGODB_URI;
+mongoose.Promise = global.Promise;
 mongoose
   .connect(dbURI)
-  .then(
-    () => console.log("MongoDB connected!"),
-    err =>
-      console.error.bind(`MongoDB connection failed, here's the error: ${err}`)
-  );
+  .then(() => console.log("MongoDB connected!"))
+  .catch(error => console.error(error));
 
 mongoose.promise = global.Promise;
 
 // Initialize Application
 const app = express();
 const port = process.env.PORT;
+
+// Sessions
+app.use(require("express-session"))({
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: false,
+  saveUninitalized: false
+});
+
+// Parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Enable API routes
 app.use("/api", api);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
 router.use((req, res, next) => {
   console.log("API Request, Time: ", Date.now()); // eslint-disable-line no-console
   next();
+});
+
+router.post("/register", (req, res) => {
+  if (req.body.username && req.body.password) {
+    const userData = {
+      username: req.body.username,
+      password: req.body.password
+    };
+
+    User.create(userData, err => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.send("User created!");
+    });
+  } else {
+    res.send("Username and Password required");
+  }
 });
 
 module.exports = router;
