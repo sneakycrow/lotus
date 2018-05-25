96b05f1ac4f5d4b4d083982d59d220b983127b5e
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-05-25 10:21:54 -0700

    removed login control from home to header

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "react-emotion";
 import { spacing } from "utils/variables";
-import Modal from "components/Modal";
-import Button from "components/Button";
 
 const Grid = styled("div")`
   display: grid;
   grid-template-columns: auto 25%;
-  padding: ${spacing.sm};
+  padding: ${spacing.xs};
 `;
 
 const Main = styled("div")`
@@ -18,37 +16,11 @@ const Sidebar = styled("div")`
   grid-column: 2;
 `;
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isModalShown: false
-    };
-    this.handleToggleModal = this.handleToggleModal.bind(this);
-  }
-
-  handleToggleModal() {
-    this.setState(state => ({ isModalShown: !state.isModalShown }));
-  }
-
-  render() {
-    return (
-      <Grid>
-        <Main>
-          Main side rendered
-          <Modal
-            isModalShown={this.state.isModalShown}
-            handleToggleModal={this.handleToggleModal}
-          >
-            Butts
-          </Modal>
-        </Main>
-        <Sidebar>
-          <Button onClick={this.handleToggleModal}>Toggle Button</Button>
-        </Sidebar>
-      </Grid>
-    );
-  }
-}
+const Home = () => (
+  <Grid>
+    <Main>Main side rendered</Main>
+    <Sidebar />
+  </Grid>
+);
 
 export default Home;
